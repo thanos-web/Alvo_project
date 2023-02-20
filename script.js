375ba996369aa31b10f30b506ca9fc88a697f1f5
Tanos <tatsday@gmail.com>
2023-02-20 12:18:47 +0300

    4

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const burger = document.querySelector(".burger")
-const menuBefore1024 = document.querySelector(".menu__before1024")
+const burger = document.querySelector(".burger");
+const menuBefore1024 = document.querySelector(".menu__before1024");
 const closeIcon = document.querySelector(".closeicon");
 burger.addEventListener('click' , () => menuBefore1024.classList.toggle("showMenu"));
-closeIcon.addEventListener('click' , () => menuBefore1024.classList.remove("showMenu"));
+closeIcon.addEventListener('click' , () => menuBefore1024.classList.remove("showMenu"),() => popupform.classList.remove("hidden"));
+
+const popupform = document.querySelector(".popup");
+const buttonwriteus = document.querySelector(".btn__mob");
+const closeiconpopupform = document.querySelector(".closeicon__popupform")
+buttonwriteus.addEventListener('click' , () => popupform.classList.toggle("hidden"));
+closeiconpopupform.addEventListener('click' ,() => popupform.classList.remove("hidden"));
+
+
