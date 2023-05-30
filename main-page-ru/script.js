5856749bba2199fc8af6d6b694ff988cc10e0b46
Tanos <tatsday@gmail.com>
2023-05-30 20:15:33 +0300

    added product page

---
+++ main-page-ru/script.js
@@ -0,0 +1,16 @@
+const burger = document.querySelector(".burger");
+const menuBefore1024 = document.querySelector(".menu__before1024");
+const closeIcon = document.querySelector(".closeicon");
+burger.addEventListener('click' , () => menuBefore1024.classList.toggle("showMenu"));
+closeIcon.addEventListener('click' , () => menuBefore1024.classList.remove("showMenu"),() => popupform.classList.remove("hidden"));
+
+const popupform = document.querySelector(".popup");
+const buttonwriteus = document.querySelector(".btn__mob");
+const closeiconpopupform = document.querySelector(".closeicon__popupform");
+const overlay = document.querySelector(".overlay");
+buttonwriteus.addEventListener('click' , () => overlay.classList.toggle("hidden"));
+buttonwriteus.addEventListener('click' , () => popupform.classList.toggle("hidden"));
+closeiconpopupform.addEventListener('click' ,() => popupform.classList.remove("hidden"));
+closeiconpopupform.addEventListener('click' ,() => overlay.classList.remove("hidden"));
+
+
